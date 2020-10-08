065d59e4ca9c5d745cda9533883ba94163717593
cami-jara <camilati.jarase@gmail.com>
2020-10-08 18:00:34 -0300

    Agregando,html, css , bootstrap y javascript

--- js/init.js
+++ js/init.js
@@ -1,74 +1,92 @@
-window.cargarTarjeta = ()=>{
-    const tarjeta = document.querySelector("#confesiones-table > tbody");
-    tarjeta.innerHTML = "";
-    for(let i=0; i < window.confesiones.length; ++i){
-        let confesionActual = window.confesiones[i];
-        let tr = document.createElement("tr");
+window.personas = [];
 
-        let tdNombre = document.createElement("td");
-        let tdTexto = document.createElement("td");
-        let tdCarrera = document.createElement("td");
-        let tdAcciones = document.createElement("td");
-        tdNombre.innerText = confesionActual.nombre;
-        tdTexto.innerHTML = confesionActual.texto;
-        tdCarrera.innerText = confesionActual.carrera;
-        let boton = document.createElement("button");
-        boton.classList.add("btn","btn-danger","btn-sm");
-        boton.innerText = "Descartar comentario";
-        boton.nroConfesion = i;
-        boton.addEventListener('click', window.eliminar);
-        tdAcciones.appendChild(boton);
-        tr.appendChild(tdNombre);
-        tr.appendChild(tdTexto);
-        tr.appendChild(tdCarrera);
-        tr.appendChild(tdAcciones);
-        tabla.appendChild(tr);  
+window.cargarPersona = ()=>{
+  let cards = document.querySelector(".cards"); 
+  cards.innerHTML="";
+  window.personas.forEach((p)=>{
+    let card = document.createElement("div");
+    card.classList.add("card","col-lg-4");
+    let cardHeader = document.createElement("div");
+    let cardBody = document.createElement("div");
+    let cardFooter = document.createElement("div");
+    
+    let nombreCard = document.createElement("span");
+    nombreCard.innerText= p.nombre;
+    let tipoCard = document.createElement("h4");
+    tipoCard.innerText="Total a pagar" + p.tipo;
+    let pagoCard = document.createElement("div");
+    let opcionCard = document.createElement("p");
+
+    if (p.pago === 'efectivo'){
+      pagoCard.style="width: 80px";
+      let imagen = document.createElement("img");
+      imagen.src = 'img/efectivo.png';
+      imagen.classList.add("img-fluid");
+      pagoCard.appendChild(imagen);
+      
+    }else{
+      pagoCard.style="width:80px";
+      let imagen = document.createElement("img");
+      imagen.src = 'img/tarjeta.png';
+      imagen.classList.add("img-fluid");
+      pagoCard.appendChild(imagen);
+
     }
+    
+    if(p.opcion === true){
+      opcionCard.innerText='Pagado';
+      cardFooter.appendChild(opcionCard);
+    }else{
+      opcionCard.innerText='Adeudado';
+      opcionCard.classList.add("text-danger");
+      cardFooter.appendChild(opcionCard);
+    } 
+    
+    cardHeader.appendChild(nombreCard);
+    cardBody.appendChild(tipoCard);
+    cardBody.appendChild(pagoCard);
+    
+    card.appendChild(cardHeader);
+    card.appendChild(cardBody);
+    card.appendChild(cardFooter);
+    cards.appendChild(card);
+  });
 }
-window.mostrarErrores = (errores)=>{
-    let erroresDiv = document.querySelector("#errores-div");
-    let ul = document.createElement("ul");
-    ul.classList.add("alert","alert-warning");
-    errores.forEach(e=>{
-        let li = document.createElement("li");
-        li.innerText = e;
-        ul.appendChild(li);
-    });
-    erroresDiv.appendChild(ul);
-};
-const boton = document.querySelector("#ingresar-btn");
 
-boton.addEventListener('click', ()=>{
+window.guardarPersona = (persona)=>{
+  window.personas.push(persona)
+  window.cargarPersona()
 
-    document.querySelector("#errores-div").innerHTML = "";
-    let nombre = document.querySelector("#nombre-txt").value.trim();
-    let texto = tinymce.get("texto-txt").getContent();
-    let carrera = document.querySelector("#carrera-select").value;
+}
 
-    let errores = [];
 
-    if(nombre === ''){
-        errores.push("Debe ingresar un nombre");
-    }
+const boton = document.getElementById("agregar-btn");
 
-    if(texto === ''){
-        errores.push("Debe ingresar la confesion");
-    }
+boton.addEventListener('click',()=>{
+  let erroresDiv = document.getElementById("errores-div");
+  
+  erroresDiv.innerHTML="";
 
-    if(errores.length === 0){
-        //Crear la confesion
-        let confesion = {};
-        confesion.nombre = nombre;
-        confesion.texto = texto;
-        confesion.carrera = carrera;
-        window.confesiones.push(confesion);
-        window.cargarTabla();
-    } else {
+ let nombre = document.getElementById("nombre-txt").value;
+ let tipo = document.getElementById("tipo-select").value;
+ let pago = document.getElementById("pago-select").value;
+ let opcion = document.getElementById("opcion-switch").checked;
 
-        //Mostrar errores
-        window.mostrarErrores(errores);
-    }
+ if(nombre === ""){
+   let error = document.createElement("p");
+   error.classList.add("alert","alert-danger");
+   error.innerText= "Debe ingresar nombre";
+   erroresDiv.appendChild(error);
 
+ }else{
+  let persona = {}
+  persona.nombre = nombre;
+  persona.tipo = tipo;
+  persona.pago = pago;
+  persona.opcion = opcion;
+
+  window.guardarPersona(persona);
+ }
 
 
 });
